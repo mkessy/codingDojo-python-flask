f08c1b7a676b6fdaf8021a3a04fa56821a3c7b26
Tyler Thibault <tyler.thibault@protonmail.com>
2021-06-18 10:07:10 -0700

    update

---
+++ Algos/W2/D5.js
@@ -0,0 +1,75 @@
+/* 
+  An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+  typically using all the original letters exactly once.
+  Is there a quick way to determine if they aren't an anagram before spending more time?
+  Given two strings
+  return whether or not they are anagrams
+*/
+
+const strA1 = "yes";
+const strB1 = "eys";
+const expected1 = true;
+
+const strA2 = "yes";
+const strB2 = "eYs";
+const expected2 = true;
+
+const strA3 = "no";
+const strB3 = "noo";
+const expected3 = false;
+
+const strA4 = "silent";
+const strB4 = "listen";
+const expected4 = true;
+
+const strA5 = ""
+const strB5 = ""
+const expected5 = true
+
+const strA6 = "123"
+const strB6 = "321"
+const expected6 = true
+
+/**
+ * Determines whether s1 and s2 are anagrams of each other.
+ * Anagrams have all the same letters but in different orders.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {boolean} Whether s1 and s2 are anagrams.
+ */
+function isAnagram(s1, s2) {}
+
+
+// *********************************************************************
+// *********************************************************************
+
+/* 
+  Given a string that may have extra spaces at the start and the end,
+  return a new string that has the extra spaces at the start and the end trimmed (removed)
+  do not remove any other spaces.
+*/
+
+const Bstr1 = "   hello world     ";
+const Bexpected1 = "hello world";
+
+const Bstr2 = "   hello world";
+const Bexpected2 = "hello world";
+
+const Bstr3 = "hello world     ";
+const Bexpected3 = "hello world";
+
+const Bstr4 = "hello   world     ";
+const Bexpected4 = "hello   world";
+
+/**
+ * Trims any leading or trailing white space from the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The given string with any leading or trailing white space
+ *    stripped.
+ */
+
+function trim(str) {}
